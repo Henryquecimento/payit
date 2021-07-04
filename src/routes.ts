b79ee131fce1b18ebc122a0d12f052e083e43767
henryque <henryque_1@hotmail.com>
2021-07-04 00:11:29 -0300

    refactor(routes): change export method

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,9 @@ import { Router } from 'express';
 
 const routes = Router();
 
+
 routes.get('/', (req, res) => {
   return res.send("Hey, it's working!");
 });
 
-export default routes;
+export { routes };
