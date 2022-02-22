ce9b31a364ab4f100606aab9f6d9a880a7d81c7f
henryque <henryque_1@hotmail.com>
2022-02-22 14:56:13 -0400

    refactor all project

--- src/app/repositories/TransactionsRepositories.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Transaction } from "../entities/Transaction";
-
-@EntityRepository(Transaction)
-class TransactionsRepositories extends Repository<Transaction> {}
-
-export { TransactionsRepositories }
