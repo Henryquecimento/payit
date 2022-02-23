44c8128af432d5a0ab5971a030f677c0f1777ef9
henryque <henryque_1@hotmail.com>
2022-02-23 16:16:18 -0400

    chore: add error handler with AppError

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,7 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import { routes } from "./routes";
 import "./app/database";
+import { AppError } from "../../errors/AppError";
 
 const app = express();
 
@@ -11,15 +12,15 @@ app.use(routes);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof Error) {
-      return response.status(400).json({
-        err: err.message,
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
       });
     }
 
     return response.status(500).json({
       status: "Error",
-      err: "Internal Server Error",
+      err: `Internal Server Error - ${err.message}`,
     });
   }
 );
