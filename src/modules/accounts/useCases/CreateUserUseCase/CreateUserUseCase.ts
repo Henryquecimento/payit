54728a2614b8a6e532f931a82ed31ae95da0ca62
henryque <henryque_1@hotmail.com>
2022-02-26 19:42:22 -0400

    feat: add CreateUserUseCase

---
+++ src/modules/accounts/useCases/CreateUserUseCase/CreateUserUseCase.ts
@@ -0,0 +1,38 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUsersDTO } from "@modules/accounts/dtos/ICreateUsersDTO";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { AppError } from "@shared/errors/AppError";
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersrepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    password,
+    email,
+    isAdmin = false,
+  }: ICreateUsersDTO): Promise<User> {
+    if (!email) throw new Error("Email incorrect!");
+
+    const userAlreadyExists = await this.usersrepository.findByEmail(email);
+
+    if (userAlreadyExists) throw new AppError("User already exists!", 400);
+
+    const user = await this.usersrepository.create({
+      name,
+      password,
+      email,
+      isAdmin,
+    });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
