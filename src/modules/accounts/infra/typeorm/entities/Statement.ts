31b29eaa38294b56e9e756db58b13fd45a8f2cc5
henryque <henryque_1@hotmail.com>
2022-03-02 19:33:00 -0400

    feat: add statement entities decorators

--- src/modules/accounts/infra/typeorm/entities/Statement.ts
+++ src/modules/accounts/infra/typeorm/entities/Statement.ts
@@ -1,19 +1,56 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+
+import { OperationType } from "@modules/accounts/enums/OperationType";
+
+import { User } from "./User";
+
+@Entity("statements")
 class Statement {
+  @PrimaryColumn()
   readonly id: string;
 
+  @Column("uuid")
   user_id: string;
 
-  sender_id: string;
+  @ManyToOne(() => User, (user) => user.statement)
+  @JoinColumn({ name: "user_id" })
+  user: User;
 
+  @Column("uuid")
+  sender_id?: string;
+
+  @ManyToOne(() => User, (user) => user.statement)
+  @JoinColumn({ name: "sender_id" })
+  sender: User;
+
+  @Column()
   description: string;
 
+  @Column("decimal")
   amount: number;
 
-  // type: OperationType; /* Create enums file */
+  @Column({ type: "enum", enum: OperationType })
+  type: OperationType;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @CreateDateColumn()
   updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
 }
 
 export { Statement };
