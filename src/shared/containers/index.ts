f37e960df46b80fd4cff86a6a424f9652df0e141
henryque <henryque_1@hotmail.com>
2022-03-05 17:13:30 -0400

    chore(containers/index): add Statements dependecy injection

--- src/shared/containers/index.ts
+++ src/shared/containers/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { StatementsRepository } from "@modules/statements/infra/typeorm/repositories/StatementsRepository";
+import { IStatementRepository } from "@modules/statements/repositories/IStatementRepository";
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
+);
+
+container.registerSingleton<IStatementRepository>(
+  "StatementsRepository",
+  StatementsRepository
 );
