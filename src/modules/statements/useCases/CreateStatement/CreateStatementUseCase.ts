5507bbe8f090948bb9ff5a226352211e17521cc6
henryque <henryque_1@hotmail.com>
2022-03-06 17:13:08 -0400

    refactor(CreateStatementUseCase): readjust to handle getUserBalance method and withdraw

--- src/modules/statements/useCases/CreateStatement/CreateStatementUseCase.ts
+++ src/modules/statements/useCases/CreateStatement/CreateStatementUseCase.ts
@@ -1,16 +1,32 @@
 import { inject, injectable } from "tsyringe";
 
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { ICreateStatementDTO } from "@modules/statements/dtos/ICreateStatement";
 import { IStatementRepository } from "@modules/statements/repositories/IStatementRepository";
+import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 class CreateStatementUseCase {
   constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
     @inject("StatementsRepository")
     private statementsRepository: IStatementRepository
   ) {}
 
   async execute({ user_id, amount, description, type }: ICreateStatementDTO) {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) throw new AppError("User not found", 400);
+
+    if (type === "withdraw") {
+      const { balance } = await this.statementsRepository.getUserBalance({
+        user_id,
+      });
+
+      if (balance < amount) throw new AppError("Insufficient Funds", 400);
+    }
+
     const statement = await this.statementsRepository.create({
       user_id,
       amount,
