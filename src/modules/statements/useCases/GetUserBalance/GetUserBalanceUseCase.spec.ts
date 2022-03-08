806d920a5652aed14e69b1336b505a6e20e9b73f
henryque <henryque_1@hotmail.com>
2022-03-08 16:14:54 -0400

    chore: add GetUserBalanceUseCase.spec test

---
+++ src/modules/statements/useCases/GetUserBalance/GetUserBalanceUseCase.spec.ts
@@ -0,0 +1,52 @@
+import { UserRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "@modules/accounts/useCases/CreateUserUseCase/CreateUserUseCase";
+import { OperationType } from "@modules/statements/enums/OperationType";
+import { StatementsRepositoryInMemory } from "@modules/statements/repositories/in-memory/StatementsRepositoryInMemory";
+
+import { GetUserBalanceUseCase } from "./GetUserBalanceUseCase";
+
+let usersRepositoryInMemory: UserRepositoryInMemory;
+let createUsersUseCase: CreateUserUseCase;
+let statementsRepositoryInMemory: StatementsRepositoryInMemory;
+let getUserBalanceUseCase: GetUserBalanceUseCase;
+
+describe("Get User Balance", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UserRepositoryInMemory();
+    createUsersUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    statementsRepositoryInMemory = new StatementsRepositoryInMemory();
+    getUserBalanceUseCase = new GetUserBalanceUseCase(
+      statementsRepositoryInMemory,
+      usersRepositoryInMemory
+    );
+  });
+
+  it("Should be able to get user's balance", async () => {
+    const user = await createUsersUseCase.execute({
+      name: "test",
+      email: "test@test.com",
+      password: "123",
+      isAdmin: true,
+    });
+
+    await statementsRepositoryInMemory.create({
+      user_id: user.id,
+      amount: 120,
+      description: "Test with Deposit",
+      type: OperationType.DEPOSIT,
+    });
+
+    await statementsRepositoryInMemory.create({
+      user_id: user.id,
+      amount: 100,
+      description: "Test with Withdraw",
+      type: OperationType.WITHDRAW,
+    });
+
+    const balance = await getUserBalanceUseCase.execute({
+      user_id: user.id,
+    });
+
+    console.log(balance);
+  });
+});
