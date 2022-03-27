bd08d52614311eb6b2e7cb6be19199b1902347fb
henryque <henryque_1@hotmail.com>
2022-03-27 14:46:13 -0400

    feat: add profile routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { UsersRoutes } from "./users.routes";
+import { UserProfileRoutes } from "./usersProfile.routes";
 
 const router = Router();
 
 router.use("/users", UsersRoutes);
+router.use("/profile", UserProfileRoutes);
 
 export { router };
