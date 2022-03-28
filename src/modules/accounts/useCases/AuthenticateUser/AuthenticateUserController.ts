b90394f7e9b5fac9f229f85b8eb9b89e67356260
henryque <henryque_1@hotmail.com>
2022-03-28 18:42:08 -0400

    feat: add AuthenticateUserController

---
+++ src/modules/accounts/useCases/AuthenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+class AuthencateUserController {
+  async handle(request: Request, response: Response) {
+    const { email, password } = request.body;
+
+    const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase);
+
+    const { user, token } = await authenticateUserUseCase.execute({
+      email,
+      password,
+    });
+
+    return response.json({ user, token });
+  }
+}
+
+export { AuthencateUserController };
