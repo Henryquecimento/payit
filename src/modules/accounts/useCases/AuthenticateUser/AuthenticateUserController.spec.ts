b8797fc65a6e65e00c81e02afccd16b010d4976d
henryque <henryque_1@hotmail.com>
2022-03-29 16:44:15 -0400

    chore: add AuthenticateUserController test

---
+++ src/modules/accounts/useCases/AuthenticateUser/AuthenticateUserController.spec.ts
@@ -0,0 +1,38 @@
+import { hash } from "bcryptjs";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm/index";
+
+describe("Authenticate User", () => {
+  let connection: Connection;
+
+  beforeAll(async () => {
+    connection = await createConnection();
+
+    await connection.runMigrations();
+
+    const id = uuid();
+    const password = await hash("admin", 8);
+
+    await connection.query(`INSERT INTO USERS(id, name, email, password, created_at, updated_at)
+    values('${id}', 'admin', 'admin@admin.com', '${password}', now(), now())`);
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("Should be able to create a user authentication", async () => {
+    const responseSession = await request(app).post("/sessions").send({
+      email: "admin@admin.com",
+      password: "admin",
+    });
+
+    expect(responseSession.status).toBe(200);
+    expect(responseSession.body).toHaveProperty("token");
+  });
+});
